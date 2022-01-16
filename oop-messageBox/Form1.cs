de16f484e33077a35ea31fdd7aa784ee2af7ac66
benedictfernando <fernandobenedict619@gmail.com>
2022-01-16 22:38:46 +0800

    remake to actual message box on load

--- oop-messageBox/Form1.cs
+++ oop-messageBox/Form1.cs
@@ -16,5 +16,12 @@ namespace oop_messageBox
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Show full name on a message box
+            if (MessageBox.Show("Benedict Fernando") == DialogResult.OK)
+                Application.Exit(); // Immediately exit application
+        }
     }
 }
